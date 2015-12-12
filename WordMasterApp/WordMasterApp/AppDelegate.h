95c5978e5a734d9f50ad502bfab90c161aa3569b
KasumiA <815888646@qq.com>
2015-12-12 11:25:30 +0800

    First commit

---
+++ WordMasterApp/WordMasterApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WordMasterApp
+//
+//  Created by Ludwig on 15/12/12.
+//  Copyright © 2015年 Ludwig. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ WordMasterApp/WordMasterApp/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  WordMasterApp
+//
+//  Created by Ludwig on 15/12/12.
+//  Copyright © 2015年 Ludwig. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
